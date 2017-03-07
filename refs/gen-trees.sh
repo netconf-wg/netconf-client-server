ce91f081d8c92fac6d322951108c452f05db4dfe
Kent Watsen <kent@watsen.net>
2017-03-07 15:37:21 -0500

    checking in what I have

--- refs/gen-trees.sh
+++ refs/gen-trees.sh
@@ -1,8 +1,4 @@
 
-pyang -p ../ -f tree ../ietf-netconf-client\@*.yang > ietf-netconf-client-tree.txt.tmp
-pyang -p ../ -f tree ../ietf-netconf-server\@*.yang > ietf-netconf-server-tree.txt.tmp
+pyang -p ../ -f tree --tree-line-length 71 ../ietf-netconf-client\@*.yang > ietf-netconf-client-tree.txt
+pyang -p ../ -f tree --tree-line-length 71 ../ietf-netconf-server\@*.yang > ietf-netconf-server-tree.txt
 
-fold -w 71 ietf-netconf-client-tree.txt.tmp > ietf-netconf-client-tree.txt
-fold -w 71 ietf-netconf-server-tree.txt.tmp > ietf-netconf-server-tree.txt
-
-rm *-tree.txt.tmp
