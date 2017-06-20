b81921a885a75f28f65142421b73935c379f5777
Kent Watsen <kent@watsen.net>
2017-06-20 12:20:29 -0400

    this is -03

--- refs/gen-trees.sh
+++ refs/gen-trees.sh
@@ -1,4 +1,4 @@
 
-pyang -p ../ -f tree --tree-line-length 71 ../ietf-netconf-client\@*.yang > ietf-netconf-client-tree.txt
+pyang -p ../ -f tree --tree-line-length 71 --tree-print-groupings ../ietf-netconf-client\@*.yang > ietf-netconf-client-tree.txt
 pyang -p ../ -f tree --tree-line-length 71 ../ietf-netconf-server\@*.yang > ietf-netconf-server-tree.txt
 
