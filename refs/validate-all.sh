2fb0015ae75652b8d7488e731c36b401622a93db
Kent Watsen <kent@watsen.net>
2018-06-05 13:36:42 -0400

    this is -06

--- refs/validate-all.sh
+++ refs/validate-all.sh
@@ -1,18 +1,20 @@
 
 echo "Testing ietf-netconf-client\@*.yang (pyang)..."
-pyang --ietf --max-line-length=70 -p ../ ../ietf-netconf-client\@*.yang
+pyang --ietf --max-line-length=69 -p ../ ../ietf-netconf-client\@*.yang
+
 echo "Testing ietf-netconf-server\@*.yang (pyang)..."
-pyang --ietf --max-line-length=70 -p ../ ../ietf-netconf-server\@*.yang
+pyang --ietf --max-line-length=69 -p ../ ../ietf-netconf-server\@*.yang
 
 echo "Testing ietf-netconf-client\@*.yang (yanglint)..."
 yanglint -p ../ ../ietf-netconf-client\@*.yang
+
 echo "Testing ietf-netconf-server\@*.yang (yanglint)..."
 yanglint -p ../ ../ietf-netconf-server\@*.yang
 
 
 echo "Testing ex-netconf-client.xml..."
-yanglint -m -p ../ -s ../ietf-netconf-client\@*.yang ex-netconf-client.xml ../../keystore/refs/ex-keystore.xml
+yanglint -m -s ../ietf-*\@*.yang ietf-origin.yang ex-netconf-client.xml ../../trust-anchors/refs/ex-trust-anchors.xml ../../keystore/refs/ex-keystore.xml
 
 echo "Testing ex-netconf-server.xml..."
-yanglint -m -p ../ -s ../ietf-netconf-server\@*.yang ietf-x509-cert-to-name@2014-12-10.yang ex-netconf-server.xml ../../keystore/refs/ex-keystore.xml
+yanglint -m -s ../ietf-*\@*.yang ietf-origin.yang ietf-x509-cert-to-name@2014-12-10.yang ex-netconf-server.xml ../../trust-anchors/refs/ex-trust-anchors.xml ../../keystore/refs/ex-keystore.xml
 
